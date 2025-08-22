77f2a9298925b281f17c839ec0d2fc6cb55c1ba8
Johandarkness <70907376+Johandarkness@users.noreply.github.com>
2025-08-22 12:50:57 -0500

    PCIDSS implementation

---
+++ internal/crypto/encryption.go
@@ -0,0 +1,58 @@
+package crypto
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "encoding/base64"
+    "errors"
+    "io"
+)
+
+type EncryptionService struct {
+    gcm cipher.AEAD
+}
+
+func NewEncryptionService(key []byte) (*EncryptionService, error) {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+    
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return nil, err
+    }
+    
+    return &EncryptionService{gcm: gcm}, nil
+}
+
+func (e *EncryptionService) Encrypt(plaintext string) (string, error) {
+    nonce := make([]byte, e.gcm.NonceSize())
+    if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+        return "", err
+    }
+    
+    ciphertext := e.gcm.Seal(nonce, nonce, []byte(plaintext), nil)
+    return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func (e *EncryptionService) Decrypt(ciphertext string) (string, error) {
+    data, err := base64.StdEncoding.DecodeString(ciphertext)
+    if err != nil {
+        return "", err
+    }
+    
+    nonceSize := e.gcm.NonceSize()
+    if len(data) < nonceSize {
+        return "", errors.New("ciphertext too short")
+    }
+    
+    nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+    plaintext, err := e.gcm.Open(nil, nonce, ciphertext, nil)
+    if err != nil {
+        return "", err
+    }
+    
+    return string(plaintext), nil
+}
---
+++ internal/crypto/keymanager.go
@@ -0,0 +1,57 @@
+package crypto
+
+import (
+    "crypto/rand"
+    "encoding/hex"
+    "fmt"
+    "sync"
+    "time"
+)
+
+type KeyManager struct {
+    currentKey    []byte
+    previousKey   []byte
+    keyVersion    int
+    rotationTime  time.Time
+    mu           sync.RWMutex
+}
+
+func NewKeyManager() *KeyManager {
+    key := make([]byte, 32) // 256 bits for AES-256
+    rand.Read(key)
+    
+    return &KeyManager{
+        currentKey:   key,
+        keyVersion:   1,
+        rotationTime: time.Now(),
+    }
+}
+
+func (km *KeyManager) GetCurrentKey() ([]byte, int) {
+    km.mu.RLock()
+    defer km.mu.RUnlock()
+    return km.currentKey, km.keyVersion
+}
+
+func (km *KeyManager) RotateKey() error {
+    km.mu.Lock()
+    defer km.mu.Unlock()
+    
+    newKey := make([]byte, 32)
+    if _, err := rand.Read(newKey); err != nil {
+        return err
+    }
+    
+    km.previousKey = km.currentKey
+    km.currentKey = newKey
+    km.keyVersion++
+    km.rotationTime = time.Now()
+    
+    return nil
+}
+
+func (km *KeyManager) GetPreviousKey() []byte {
+    km.mu.RLock()
+    defer km.mu.RUnlock()
+    return km.previousKey
+}
